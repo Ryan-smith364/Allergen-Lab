5924f9c5b73778ebbbcb5cf2def63615947cac32
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-11-26 11:04:46 -0500

    seed_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ tomato    = Ingredient.create(name: "Tomato")
+ onion     = Ingredient.create(name: "Onion")
+ peanut    = Ingredient.create(name: "Peanut")
+
+rec-ing1   = Recipeingredient.create(user_id: user2, ingredient_id: tomato)
+rec-ing2   = Recipeingredient.create(user_id: user3 , ingredient_id: onion)
+rec_ing3   = Recipeingredient.create(user_id: user1, ingredient_id: peanut)
+rec-ing4   = Recipeingredient.create(user_id: user2, ingredient_id: onion)
+
+allergen1  = Allergen.create(ingredient_id: tomato, user_id: user1) 
+allegen2   = Allergen.create(ingredient_id: onion, user_id: user3)
